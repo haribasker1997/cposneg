758157606d6735992897fd7343a11596b0a39b9b
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2017-09-02 18:38:03 +0530

    Add files via upload

---
+++ new 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+{
+float num;
+printf("enter the number");
+scanf("%d",&num);
+if(num>0)
+printf("positive number");
+elseif(num==0)
+printf("zero");
+else
+printf("negative number");
+}
